865c0d85c536925b2e57ffe9bc95995834f6dd2d
snow-sr <joaofelipi877@gmail.com>
2022-12-02 10:24:13 -0300

    Exemplo cachorro posição

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import dog from "./components/dog.vue";
+</script>
+
+<template>
+  <main class="mapa">
+    <dog name="fumaça" :position="[230, 240]"></dog>
+    <dog name="Maicon" :position="[540, 600]"></dog>
+  </main>
+</template>
+
+<style scoped>
+/* Background image assets/mapa */
+.mapa {
+  background-image: url("./assets/ifcmapatt.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 100vh;
+  width: 100vw;
+}
+</style>
---
+++ src/components/dog.vue
@@ -0,0 +1,34 @@
+<!-- eslint-disable-next-line vue/multi-word-component-names -->
+<template>
+  <main :style="style">
+    <h1>{{ name }}</h1>
+    <p>{{ position }}</p>
+  </main>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    position: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  computed: {
+    style() {
+      return `transform: translate(${this.position[0]}px, ${this.position[1]}px)`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+* {
+  color: RED;
+}
+</style>
